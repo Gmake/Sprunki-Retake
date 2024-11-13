da5673c6e9edc2b22dfa98cf3dbc10029b772d06
Gmake <hent361@163.com>
2024-11-13 10:09:55 +0800

    修改尺寸

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,11 +19,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon.ico', sizes: 'any' },
-      { url: '/image/icon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/image/icon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/image/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/image/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
     ],
     apple: [
-      { url: '/image/apple-icon.png', sizes: '180x180', type: 'image/png' },
+      { url: '/image/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
     ],
   },
   openGraph: {
